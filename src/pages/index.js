09e6fe730c9a8141e673f918d249419551175434
Brock Fanning <brockfanning@gmail.com>
2019-01-02 14:32:28 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class IndexPage extends React.Component {
+
+  render() {
+    const pages = this.props.data.allNodePage.edges
+    const pageTitles = pages.map(page => <li>{page.node.title}</li>)
+    return <ul>{pageTitles}</ul>
+  }
+}
+
+export default IndexPage
+
+export const query = graphql`
+  query pageQuery {
+    allNodePage {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
