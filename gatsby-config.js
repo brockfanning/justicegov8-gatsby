62fb4e2920b4414862e6587b00be482a8dab08d1
Brock Fanning <brockfanning@gmail.com>
2019-01-02 16:15:04 -0500

    Clean up the starter stuff and import USWDS styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Justice.gov with Gatsby and Drupal 8`,
+    description: `This is a proof-of-concept using Gatsby and Drupal 8 to implement www.justice.gov.`,
+    author: `@brockfanning`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: `gatsby-source-drupal`,
       options: {
         baseUrl: `http://local.justice8.doj.gov`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
-class IndexPage extends React.Component {
+import Layout from "../components/layout"
+
+class HomePage extends React.Component {
 
   render() {
     const pages = this.props.data.allNodePage.edges
     const pageTitles = pages.map(page => <li>{page.node.title}</li>)
-    return <ul>{pageTitles}</ul>
+    return (
+      <Layout>
+        <ul>{pageTitles}</ul>
+      </Layout>
+    )
   }
 }
 
-export default IndexPage
+export default HomePage
 
 export const query = graphql`
   query pageQuery {
