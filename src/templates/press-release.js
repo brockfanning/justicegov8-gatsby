c6cb81c43badafb1d1f80b41f5698caf08c34bd0
Brock Fanning <brockfanning@gmail.com>
2019-01-03 15:42:03 -0500

    Use react-html-parser to read Drupal content

--- src/templates/press-release.js
+++ src/templates/press-release.js
@@ -1,12 +1,13 @@
 import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../components/layout"
+import ReactHtmlParser from "react-html-parser";
 
 const PressReleaseDetail = ({ data }) => (
   <Layout>
     <section className="grid-container">
       <h2>{ data.nodePressRelease.title }</h2>
-      { data.nodePressRelease.body.value }
+      { ReactHtmlParser(data.nodePressRelease.body.value) }
     </section>
   </Layout>
 )
